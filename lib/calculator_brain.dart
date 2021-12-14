276b61801fc97ebd861d9b06adcb8096970f267a
Kayes Ashfaq <asfak7074@gmail.com>
2021-12-14 17:51:04 +0600

    minor bugs fixed

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -3,19 +3,19 @@ import 'dart:math';
 class CalculatorBrain {
   final int height;
   final int weight;
-  double? _bmi;
+  double _bmi = 0;
 
   CalculatorBrain({required this.height, required this.weight});
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
-    return _bmi!.toStringAsFixed(1);
+    return _bmi.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi! > 25) {
+    if (_bmi > 25) {
       return 'OVERWEIGHT';
-    } else if (_bmi! < 18.5) {
+    } else if (_bmi > 18.5) {
       return 'NORMAL';
     } else {
       return 'UNDERWEIGHT';
@@ -23,9 +23,9 @@ class CalculatorBrain {
   }
 
   String getInterpretation() {
-    if (_bmi! > 25) {
+    if (_bmi > 25) {
       return 'You have a higher then normal body weight! Try to exercise more.';
-    } else if (_bmi! < 18.5) {
+    } else if (_bmi > 18.5) {
       return 'You have normal body weight! Good Job.';
     } else {
       return 'You have lower then normal body weight! You can eat a bit more.';
